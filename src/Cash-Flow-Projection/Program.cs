e8fa6b38bfa9b3f7dd94c891752b54a49677cbe4
Matt Wagner <mattgwagner@gmail.com>
2019-12-06 17:37:22 -0500

    Update Program.cs

--- src/Cash-Flow-Projection/Program.cs
+++ src/Cash-Flow-Projection/Program.cs
@@ -1,21 +1,20 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using System.IO;
-
 namespace Cash_Flow_Projection
 {
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .CaptureStartupErrors(true)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
