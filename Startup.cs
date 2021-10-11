e968a7f370469b57ac21fb230a9ffa6a5de03335
Matt Wagner <Matt@RedLeg.dev>
2021-10-10 20:55:41 -0400

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -26,8 +26,6 @@ namespace Cash_Flow_Projection
             Configuration = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
